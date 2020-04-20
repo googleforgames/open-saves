dcabde0ea0fbf9d53122cf2e367fe4c462899223
Emma Haruka Iwao <yuryu@google.com>
2020-04-20 10:31:29 -0700

    add initial API definition in proto (#6)
    * feat: add init proto
    
    * chore: generate genproto and swagger files
    
    * add additional proto definitions for kv pairs and json
    
    * refactor resource definitions and add gen scripts
    
    * rename data objects as Record
    
    * rename data objects as Record
    
    * update gen scripts
    
    * add various bazel rules
    
    * working Build files for protos
    
    * add grpc-gateway to go.mod
    
    * add swagger gen rule to api/BUILD
    
    * Add explicit builidifer dep to WORKSPACE
    
    * Change Cloud Build machine type to n1-highcpu-8
    
    Co-authored-by: Alex Hong <hongalex@google.com>

---
+++ api/gengateway.sh
@@ -0,0 +1,5 @@
+protoc -I/usr/local/include -I. \
+  -I$GOPATH/src \
+  -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
+  --grpc-gateway_out=logtostderr=true:. \
+  triton.proto
