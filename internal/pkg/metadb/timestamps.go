a807dc392662ed9719b44f4d99311541475dfbaa
Emma Haruka Iwao <yuryu@google.com>
2020-07-20 13:15:01 -0700

    feat: Timestamps now implements PropertyLoadSaver

--- internal/pkg/metadb/timestamps.go
+++ internal/pkg/metadb/timestamps.go
@@ -17,13 +17,20 @@ package metadb
 import (
 	"time"
 
+	"cloud.google.com/go/datastore"
 	"github.com/google/uuid"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
-// timestamps keeps keeps when each record is created or updated as well as
+const (
+	signaturePropertyName = "Signature"
+)
+
+// Timestamps keeps keeps when each record is created or updated as well as
 // a randomly generated UUID to keep consistency under concurrent writes.
 // It should be embedded to metadata entities such as Record and Store.
-type timestamps struct {
+type Timestamps struct {
 	// CreatedAt is the timestamp of the record creation time
 	// Automatically set by MetaDB
 	CreatedAt time.Time
@@ -32,11 +39,14 @@ type timestamps struct {
 	UpdatedAt time.Time
 	// Signature is a UUID that is randomly created each time the record is updated
 	// Automatically set and managed by MetaDB
-	Signature uuid.UUID `datastore:",noindex"`
+	Signature uuid.UUID `datastore:"-"`
 }
 
+// Assert Timestamps implements the PropertyLoadSaver interface.
+var _ datastore.PropertyLoadSaver = new(Timestamps)
+
 // NewTimestamps sets CreatedAt and UpdatedAt to time.Now() and Signature to uuid.New().
-func (t *timestamps) NewTimestamps() {
+func (t *Timestamps) NewTimestamps() {
 	now := time.Now()
 	t.CreatedAt = now
 	t.UpdatedAt = now
@@ -44,7 +54,46 @@ func (t *timestamps) NewTimestamps() {
 }
 
 // UpdateTimestamps updates the UpdatedAt and Signature fields with time.Now() and uuid.New().
-func (t *timestamps) UpdateTimestamps() {
+func (t *Timestamps) UpdateTimestamps() {
 	t.UpdatedAt = time.Now()
 	t.Signature = uuid.New()
+}
+
+// Load implements the Datastore PropertyLoadSaver interface and converts Datastore
+// properties to corresponding struct fields.
+func (t *Timestamps) Load(ps []datastore.Property) error {
+	for i, p := range ps {
+		if p.Name == signaturePropertyName {
+			if s, ok := p.Value.(string); ok {
+				sig, err := uuid.Parse(s)
+				if err != nil {
+					return err
+				}
+				t.Signature = sig
+				// Signature needs to be removed from ps before passed to LoadStruct.
+				// This overwrites the ps argument but it seems fine with the current library.
+				ps[i] = ps[len(ps)-1]
+				ps = ps[:len(ps)-1]
+				break
+			} else {
+				return status.Errorf(codes.Internal, "Signature property is not string: %+v", p.Value)
+			}
+		}
+	}
+	return datastore.LoadStruct(t, ps)
+}
+
+// Save implements the Datastore PropertyLoadSaver interface and converts the properties
+// field in the struct to separate Datastore properties.
+func (t *Timestamps) Save() ([]datastore.Property, error) {
+	ps, err := datastore.SaveStruct(t)
+	if err != nil {
+		return nil, err
+	}
+	ps = append(ps, datastore.Property{
+		Name:    signaturePropertyName,
+		Value:   t.Signature.String(),
+		NoIndex: true,
+	})
+	return ps, nil
 }
--- internal/pkg/metadb/timestamps_test.go
+++ internal/pkg/metadb/timestamps_test.go
@@ -18,12 +18,14 @@ import (
 	"testing"
 	"time"
 
+	"cloud.google.com/go/datastore"
+	"github.com/google/uuid"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestTimestamps_NewTimestamps(t *testing.T) {
 	beforeNew := time.Now()
-	var ts timestamps
+	var ts Timestamps
 	ts.NewTimestamps()
 	afterNew := time.Now()
 	assert.NotNil(t, ts)
@@ -39,14 +41,14 @@ func TestTimestamps_NewTimestamps(t *testing.T) {
 }
 
 func TestTimestamps_Update(t *testing.T) {
-	var ts timestamps
+	var ts Timestamps
 	ts.NewTimestamps()
 	ocreated := ts.CreatedAt
 	oupdated := ts.UpdatedAt
 	osignature := ts.Signature
 	assert.NotNil(t, ts)
 	beforeUpdate := time.Now()
-	ts.Update()
+	ts.UpdateTimestamps()
 	afterUpdate := time.Now()
 
 	assert.True(t, ocreated.Equal(ts.CreatedAt))
@@ -54,4 +56,56 @@ func TestTimestamps_Update(t *testing.T) {
 	assert.True(t, beforeUpdate.Before(ts.UpdatedAt))
 	assert.True(t, afterUpdate.After(ts.UpdatedAt))
 	assert.NotEqual(t, osignature, ts.Signature)
+}
+
+func TestTimestamps_Save(t *testing.T) {
+	var ts Timestamps
+	ts.NewTimestamps()
+	actual, err := ts.Save()
+	assert.NoError(t, err)
+	expected := []datastore.Property{
+		{
+			Name:  "CreatedAt",
+			Value: ts.CreatedAt,
+		},
+		{
+			Name:  "UpdatedAt",
+			Value: ts.UpdatedAt,
+		},
+		{
+			Name:    signaturePropertyName,
+			Value:   ts.Signature.String(),
+			NoIndex: true,
+		},
+	}
+	assert.Equal(t, expected, actual)
+}
+
+func TestTimestamps_Load(t *testing.T) {
+	createdAt := time.Now()
+	updatedAt := createdAt.Add(time.Hour)
+	uuid := uuid.New()
+	properties := []datastore.Property{
+		{
+			Name:  "CreatedAt",
+			Value: createdAt,
+		},
+		{
+			Name:  "UpdatedAt",
+			Value: updatedAt,
+		},
+		{
+			Name:    signaturePropertyName,
+			Value:   uuid.String(),
+			NoIndex: true,
+		},
+	}
+	var actual Timestamps
+	assert.NoError(t, actual.Load(properties))
+	expected := Timestamps{
+		CreatedAt: createdAt,
+		UpdatedAt: updatedAt,
+		Signature: uuid,
+	}
+	assert.Equal(t, expected, actual)
 }
