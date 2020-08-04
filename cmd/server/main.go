16bb1811371fa26f52f48586bf2415576a51b15b
Alex Hong <hongalex@google.com>
2020-08-03 19:09:40 -0700

    add comment about cloud run and port

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -56,6 +56,7 @@ func main() {
 		Cache:   *cache,
 	}
 
+	// Cloud Run environment populates the PORT env var, so check for it here.
 	if p := os.Getenv("PORT"); p != "" {
 		p, err := strconv.ParseUint(p, 10, 64)
 		if err != nil {
