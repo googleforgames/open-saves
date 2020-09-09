03cdd90495242fdaac3aa2419831796b5bc1e143
Emma Haruka Iwao <yuryu@google.com>
2020-09-09 16:34:53 -0700

    feat: Add streaming methods to BlobStore (#120)
    This PR adds streaming methods to the BlobStore interface to support larger objects.

--- internal/pkg/blob/blob.go
+++ internal/pkg/blob/blob.go
@@ -14,10 +14,31 @@
 
 package blob
 
-import "context"
+import (
+	"context"
+	"io"
+)
 
+// BlobStore is a public interface for Blob operations within Triton.
+// Use one of the structs defined in the package.
+// Currently available drivers:
+// 	- BlobGCP: Google Cloud Storage
 type BlobStore interface {
 	Put(ctx context.Context, path string, data []byte) error
+
+	// NewWriter creates a new object with path and returns an io.WriteCloser
+	// instance for the object.
+	// Make sure to close the writer after all operations to the writer.
+	NewWriter(ctx context.Context, path string) (io.WriteCloser, error)
 	Get(ctx context.Context, path string) ([]byte, error)
+
+	// NewReader is an alias to NewRangeReader(ctx, path, 0, -1), which creates
+	// a reader from the beginning of an object to EOF.
+	NewReader(ctx context.Context, path string) (io.ReadCloser, error)
+
+	// NewRangeReader returns an io.ReadCloser instance for the object specified by path,
+	// beginning at the offset-th byte and length bytes long. length = -1 means until EOF.
+	// Make sure to close the reader after all operations to the reader.
+	NewRangeReader(ctx context.Context, path string, offset, length int64) (io.ReadCloser, error)
 	Delete(ctx context.Context, path string) error
 }
