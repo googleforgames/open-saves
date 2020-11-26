f9eb346f5fe40ad38a62290a2b2a67c6aa273fa1
Emma Haruka Iwao <yuryu@google.com>
2020-11-25 16:18:17 -0800

    feature: add a blob streaming APIs
    - Add CreateBlob, GetBlob, DeleteBlob to the api proto
    - Rename metadb.Blob to BlobRef to avoid confusion
    - Add StoreKey And RecordKey to BlobRef
    - Fix a BlobRef state transition bug

--- internal/pkg/metadb/blob_test.go
+++ internal/pkg/metadb/blob_test.go
@@ -19,132 +19,178 @@ import (
 
 	"cloud.google.com/go/datastore"
 	"github.com/google/uuid"
-	"github.com/googleforgames/open-saves/internal/pkg/metadb"
+	m "github.com/googleforgames/open-saves/internal/pkg/metadb"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestBlob_LoadKey(t *testing.T) {
-	blob := new(metadb.Blob)
+func TestBlobRef_LoadKey(t *testing.T) {
 	key := uuid.MustParse("d13c289c-8845-485f-b582-c87342d5dade")
-	assert.NoError(t, blob.LoadKey(datastore.NameKey("kind", key.String(), nil)))
+	blob := new(m.BlobRef)
+	assert.NoError(t, blob.LoadKey(datastore.NameKey("blob", key.String(), nil)))
 	assert.Equal(t, key, blob.Key)
 }
 
-func TestBlob_Save(t *testing.T) {
-	blob := metadb.Blob{
-		Key:        uuid.New(),
-		Size:       123,
-		ObjectName: "object name",
-		Status:     metadb.BlobStatusInitializing,
+func TestBlobRef_Save(t *testing.T) {
+	const (
+		size       = int64(123)
+		objectName = "object name"
+		store      = "store"
+		record     = "record"
+	)
+
+	blob := m.BlobRef{
+		Size:       size,
+		ObjectName: objectName,
+		Status:     m.BlobRefStatusInitializing,
+		StoreKey:   store,
+		RecordKey:  record,
 	}
 	expected := []datastore.Property{
 		{
 			Name:  "Size",
-			Value: int64(123),
+			Value: size,
 		},
 		{
 			Name:  "ObjectName",
-			Value: "object name",
+			Value: objectName,
 		},
 		{
 			Name:  "Status",
-			Value: int64(metadb.BlobStatusInitializing),
+			Value: int64(m.BlobRefStatusInitializing),
+		},
+		{
+			Name:  "StoreKey",
+			Value: store,
+		},
+		{
+			Name:  "RecordKey",
+			Value: record,
 		},
 	}
 	actual, err := blob.Save()
 	assert.NoError(t, err)
 	if assert.NotNil(t, actual) {
 		assert.Equal(t, expected, actual[:len(expected)])
-		assert.Equal(t, 4, len(actual))
-		assert.Equal(t, "Timestamps", actual[3].Name)
+		assert.Equal(t, 6, len(actual))
+		assert.Equal(t, "Timestamps", actual[5].Name)
 	}
 }
 
-func TestBlob_Load(t *testing.T) {
+func TestBlobRef_Load(t *testing.T) {
+	const (
+		size       = int64(123)
+		objectName = "object name"
+		store      = "store"
+		record     = "record"
+	)
 	properties := []datastore.Property{
 		{
 			Name:  "Size",
-			Value: int64(123),
+			Value: size,
 		},
 		{
 			Name:  "ObjectName",
-			Value: "object name",
+			Value: objectName,
 		},
 		{
 			Name:  "Status",
-			Value: int64(metadb.BlobStatusReady),
+			Value: int64(m.BlobRefStatusReady),
+		},
+		{
+			Name:  "StoreKey",
+			Value: store,
+		},
+		{
+			Name:  "RecordKey",
+			Value: record,
 		},
 	}
-	expected := &metadb.Blob{
+	expected := &m.BlobRef{
 		Size:       123,
 		ObjectName: "object name",
-		Status:     metadb.BlobStatusReady,
+		Status:     m.BlobRefStatusReady,
+		StoreKey:   store,
+		RecordKey:  record,
 	}
-	actual := new(metadb.Blob)
+	actual := new(m.BlobRef)
 	err := actual.Load(properties)
 	if assert.NoError(t, err) {
 		assert.Equal(t, expected, actual)
 	}
 }
 
-func newInitBlob(t *testing.T) *metadb.Blob {
-	blob := new(metadb.Blob)
+func newInitBlob(t *testing.T) *m.BlobRef {
+	blob := new(m.BlobRef)
 	const (
-		size = int64(4)
-		name = "abc"
+		size   = int64(4)
+		name   = "abc"
+		store  = "store"
+		record = "record"
 	)
 
 	// Initialize
-	assert.NoError(t, blob.Initialize(size, name))
+	assert.NoError(t, blob.Initialize(size, store, record, name))
 	assert.NotEqual(t, uuid.Nil, blob.Key)
 	assert.Equal(t, size, blob.Size)
 	assert.Equal(t, name, blob.ObjectName)
-	assert.Equal(t, metadb.BlobStatusInitializing, blob.Status)
+	assert.Equal(t, m.BlobRefStatusInitializing, blob.Status)
+	assert.Equal(t, store, blob.StoreKey)
+	assert.Equal(t, record, blob.RecordKey)
 	assert.NotEmpty(t, blob.Timestamps.CreatedAt)
 	assert.NotEmpty(t, blob.Timestamps.UpdatedAt)
 	assert.NotEmpty(t, blob.Timestamps.Signature)
 	return blob
 }
 
-func TestBlob_LifeCycle(t *testing.T) {
+func TestBlobRef_LifeCycle(t *testing.T) {
 	blob := newInitBlob(t)
 
-	// Invalid transitions
-	assert.Error(t, blob.Initialize(0, ""))
-	assert.Error(t, blob.Retire())
+	// Invalid transition
+	assert.Error(t, blob.Initialize(0, "", "", ""))
+
+	// Mark for deletion
+	assert.NoError(t, blob.MarkForDeletion())
+	assert.Equal(t, m.BlobRefStatusPendingDeletion, blob.Status)
+
+	// Start over
+	blob = newInitBlob(t)
 
 	// Ready
 	assert.NoError(t, blob.Ready())
-	assert.Equal(t, metadb.BlobStatusReady, blob.Status)
+	assert.Equal(t, m.BlobRefStatusReady, blob.Status)
 
 	// Invalid transitions
-	assert.Error(t, blob.Initialize(0, ""))
+	assert.Error(t, blob.Initialize(0, "", "", ""))
 	assert.Error(t, blob.Ready())
 
-	// Retire
-	assert.NoError(t, blob.Retire())
-	assert.Equal(t, metadb.BlobStatusPendingDeletion, blob.Status)
+	// Mark for deletion
+	assert.NoError(t, blob.MarkForDeletion())
+	assert.Equal(t, m.BlobRefStatusPendingDeletion, blob.Status)
 
 	// Invalid transitions
-	assert.Error(t, blob.Retire())
+	assert.Error(t, blob.MarkForDeletion())
 	assert.Error(t, blob.Ready())
-	assert.Error(t, blob.Initialize(0, ""))
+	assert.Error(t, blob.Initialize(0, "", "", ""))
 }
 
-func TestBlob_Fail(t *testing.T) {
-	blob := new(metadb.Blob)
+func TestBlobRef_Fail(t *testing.T) {
+	blob := new(m.BlobRef)
 
 	// Fail should fail for BlobStatusUnknown
 	assert.Error(t, blob.Fail())
 
 	blob = newInitBlob(t)
 	assert.NoError(t, blob.Fail())
-	blob.Status = metadb.BlobStatusPendingDeletion
+
+	blob.Status = m.BlobRefStatusInitializing
 	assert.NoError(t, blob.Fail())
 
-	blob.Status = metadb.BlobStatusReady
+	blob.Status = m.BlobRefStatusPendingDeletion
+	assert.NoError(t, blob.Fail())
+
+	blob.Status = m.BlobRefStatusReady
 	assert.Error(t, blob.Fail())
 
-	blob.Status = metadb.BlobStatusError
+	blob.Status = m.BlobRefStatusError
 	assert.Error(t, blob.Fail())
 }
