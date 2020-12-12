e2c00883923421b90589a6e249d408b8960ba327
Emma Haruka Iwao <yuryu@google.com>
2020-12-11 18:04:59 -0800

    test: Move assertEqual{Store,Record} to util pkg

---
+++ internal/pkg/metadb/metadbtest/metadbtest_test.go
@@ -0,0 +1,80 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package metadbtest
+
+import (
+	"testing"
+	"time"
+
+	"github.com/google/uuid"
+	pb "github.com/googleforgames/open-saves/api"
+	"github.com/googleforgames/open-saves/internal/pkg/metadb"
+)
+
+func TestMetaDBTest_TestAssertEqualStore(t *testing.T) {
+	AssertEqualStore(t, nil, nil)
+	AssertEqualStoreWithinDuration(t, nil, nil, time.Duration(100))
+	store := &metadb.Store{
+		Key:     "abc",
+		OwnerID: "owner",
+		Tags:    []string{"tag1", "tag2"},
+		Timestamps: metadb.Timestamps{
+			CreatedAt: time.Unix(100, 0),
+			UpdatedAt: time.Unix(110, 0),
+			Signature: uuid.MustParse("BF6B705A-D14E-414D-A40F-58F6311119B9"),
+		},
+	}
+	AssertEqualStore(t, store, store)
+	store2 := new(metadb.Store)
+	*store2 = *store
+	store2.Tags = []string{"tag2", "tag1"}
+	store2.Timestamps.CreatedAt = store2.Timestamps.CreatedAt.Add(time.Duration(42))
+	store2.Timestamps.UpdatedAt = store2.Timestamps.UpdatedAt.Add(time.Duration(42))
+	AssertEqualStoreWithinDuration(t, store, store2, time.Duration(42))
+}
+
+func TestMetaDBTest_TestAssertEqualRecord(t *testing.T) {
+	AssertEqualRecord(t, nil, nil)
+	AssertEqualRecordWithinDuration(t, nil, nil, time.Duration(100))
+	record := &metadb.Record{
+		Key:      "abc",
+		Blob:     []byte{1, 2, 3},
+		BlobSize: 3,
+		OwnerID:  "owner",
+		Tags:     []string{"tag1", "tag2"},
+		Properties: metadb.PropertyMap{
+			"prop1": {
+				Type:         pb.Property_INTEGER,
+				IntegerValue: 42,
+			},
+			"prop2": {
+				Type:        pb.Property_STRING,
+				StringValue: "string value",
+			},
+		},
+		Timestamps: metadb.Timestamps{
+			CreatedAt: time.Unix(100, 0),
+			UpdatedAt: time.Unix(110, 0),
+			Signature: uuid.MustParse("BF6B705A-D14E-414D-A40F-58F6311119B9"),
+		},
+	}
+	AssertEqualRecord(t, record, record)
+	record2 := new(metadb.Record)
+	*record2 = *record
+	record2.Tags = []string{"tag2", "tag1"}
+	record2.Timestamps.CreatedAt = record2.Timestamps.CreatedAt.Add(time.Duration(42))
+	record2.Timestamps.UpdatedAt = record2.Timestamps.UpdatedAt.Add(time.Duration(42))
+	AssertEqualRecordWithinDuration(t, record, record2, time.Duration(42))
+}
