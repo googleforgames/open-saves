a86f3e25a5221fe835b8150ff17f805d23b26ed5
Emma Haruka Iwao <yuryu@google.com>
2021-01-14 11:37:23 -0800

    feat: use empty string for nil uuid (#146)
    Use empty strings to represent nil UUIDs in the datastore to save storage space when we use these methods to store UUIDs for blob references in the Record objects.
    This PR also moves the uuid property handler to a new file for better structure.

---
+++ internal/pkg/metadb/uuid.go
@@ -0,0 +1,64 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package metadb
+
+import (
+	"cloud.google.com/go/datastore"
+	"github.com/google/uuid"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+func uuidToDatastoreProperty(name string, u uuid.UUID, noIndex bool) datastore.Property {
+	// Output an empty string instead of "000000-..." to save space
+	if u == uuid.Nil {
+		return datastore.Property{
+			Name:    name,
+			Value:   "",
+			NoIndex: noIndex,
+		}
+	}
+	return datastore.Property{
+		Name:    name,
+		Value:   u.String(),
+		NoIndex: noIndex,
+	}
+}
+
+func datastoreLoadUUID(ps []datastore.Property, name string) (uuid.UUID, []datastore.Property, error) {
+	for i, p := range ps {
+		if p.Name == name {
+			if s, ok := p.Value.(string); ok {
+				// The property needs to be removed from ps before passed to LoadStruct.
+				// This overwrites the slice but it seems fine with the current library.
+				ps[i] = ps[len(ps)-1]
+				ps = ps[:len(ps)-1]
+
+				if s == "" {
+					// Treat empty strings as the nil UUID
+					return uuid.Nil, ps, nil
+				}
+				sig, err := uuid.Parse(s)
+				if err != nil {
+					return uuid.Nil, ps, err
+				}
+
+				return sig, ps, nil
+			}
+			return uuid.Nil, ps, status.Errorf(codes.Internal, "UUID property is not string: %+v", p.Value)
+		}
+	}
+	return uuid.Nil, ps, status.Errorf(codes.Internal, "UUID property is not found, name = %v", name)
+}
