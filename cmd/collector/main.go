ff02dc33409783a7c8927c1bc048f157cc144af9
Emma Haruka Iwao <yuryu@google.com>
2021-02-18 15:13:19 -0800

    feat: make collect-before configurable

--- cmd/collector/main.go
+++ cmd/collector/main.go
@@ -3,24 +3,26 @@ package main
 import (
 	"context"
 	"flag"
-	"os"
-	"strconv"
+	"time"
 
 	"github.com/googleforgames/open-saves/internal/app/collector"
+	"github.com/googleforgames/open-saves/internal/pkg/cmd"
 	log "github.com/sirupsen/logrus"
 )
 
 func main() {
-	defaultCloud := getEnvVarString("OPEN_SAVES_CLOUD", "gcp")
-	defaultBucket := getEnvVarString("OPEN_SAVES_BUCKET", "gs://triton-dev-store")
-	defaultProject := getEnvVarString("OPEN_SAVES_PROJECT", "triton-for-games-dev")
-	defaultCache := getEnvVarString("OPEN_SAVES_CACHE", "localhost:6379")
+	defaultCloud := cmd.GetEnvVarString("OPEN_SAVES_CLOUD", "gcp")
+	defaultBucket := cmd.GetEnvVarString("OPEN_SAVES_BUCKET", "gs://triton-dev-store")
+	defaultProject := cmd.GetEnvVarString("OPEN_SAVES_PROJECT", "triton-for-games-dev")
+	defaultCache := cmd.GetEnvVarString("OPEN_SAVES_CACHE", "localhost:6379")
+	defaultBefore := cmd.GetEnvVarDuration("OPEN_SAVES_COLLECT_BEFORE", 24*time.Hour)
 
 	var (
 		cloud   = flag.String("cloud", defaultCloud, "The public cloud provider you wish to run Open Saves on")
 		bucket  = flag.String("bucket", defaultBucket, "The bucket which will hold Open Saves blobs")
 		project = flag.String("project", defaultProject, "The GCP project ID to use for Datastore")
 		cache   = flag.String("cache", defaultCache, "The address of the cache store instance")
+		before  = flag.Duration("collect-before", defaultBefore, "Collector deletes entries older than this time.Duration value (e.g. \"24h\")")
 	)
 
 	flag.Parse()
@@ -42,27 +44,9 @@ func main() {
 		Bucket:  *bucket,
 		Project: *project,
 		Cache:   *cache,
+		Before:  time.Now().Add(-*before),
 	}
 
 	ctx := context.Background()
 	collector.Run(ctx, cfg)
-}
-
-func getEnvVarUInt(name string, defValue uint64) uint64 {
-	if value := os.Getenv(name); value != "" {
-		uval, err := strconv.ParseUint(value, 10, 64)
-		if err != nil {
-			log.Warningf("failed to parse %s env variable, default to %v", name, defValue)
-			uval = defValue
-		}
-		return uval
-	}
-	return defValue
-}
-
-func getEnvVarString(name string, defValue string) string {
-	if value := os.Getenv(name); value != "" {
-		return value
-	}
-	return defValue
 }
