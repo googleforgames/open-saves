2fd807effe3b5a330b40b0726bda70744aa39c30
Emma Haruka Iwao <yuryu@google.com>
2021-05-24 16:25:34 -0700

    test: fix test

--- internal/pkg/metadb/blobref/chunkref/chunkref_test.go
+++ internal/pkg/metadb/blobref/chunkref/chunkref_test.go
@@ -27,8 +27,8 @@ func TestChunkRef_New(t *testing.T) {
 	c := New(blobuuid, 42, 123450)
 	assert.NotEqual(t, uuid.Nil, c.Key)
 	assert.Equal(t, blobuuid, c.BlobRef)
-	assert.Equal(t, 42, c.Number)
-	assert.Equal(t, 123450, c.Size)
+	assert.Equal(t, int32(42), c.Number)
+	assert.Equal(t, int32(123450), c.Size)
 }
 
 func TestChunkRef_ObjectPath(t *testing.T) {
