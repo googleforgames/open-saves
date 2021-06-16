4d0c3cc1a942df34ec028dfc3f96c730285bedce
Emma Haruka Iwao <yuryu@google.com>
2021-06-16 15:26:43 -0700

    refactor: Separate blobref.Status (#261)
    Separte blobref.Status as a new type to later use in ChunkRef

--- internal/pkg/metadb/blobref/blobref.go
+++ internal/pkg/metadb/blobref/blobref.go
@@ -15,45 +15,9 @@
 package blobref
 
 import (
-	"errors"
-
 	"cloud.google.com/go/datastore"
 	"github.com/google/uuid"
 	"github.com/googleforgames/open-saves/internal/pkg/metadb/timestamps"
-)
-
-// Status represents a current blob status.
-//
-// Life of a Blob
-//
-// [New Record created] --> [new BlobRef entity with BlobRefStatusInitializing]
-//                         /               \
-//                        / fail            \  success
-//                       v                   v
-//                [BlobRefStatusError]       [BlobRefStatusReady]
-//                       |       x            |
-//      Upload new blob  |        \ fail      | Record deleted or new blob uploaded
-//            or         |         \          v
-//     delete the record |          -----[BlobRefStatusPendingDeletion]
-//                       v                  /
-//  [Delete the blob entity] <-------------/   Garbage collection
-type Status int16
-
-const (
-	// StatusUnknown represents internal error.
-	StatusUnknown Status = iota
-	// StatusInitializing means the blob is currently being prepared, i.e.
-	// being uploaded to the blob store.
-	StatusInitializing
-	// StatusReady means the blob is committed and ready for use.
-	StatusReady
-	// StatusPendingDeletion means the blob is no longer referenced by
-	// any Record entities and needs to be deleted.
-	StatusPendingDeletion
-	// StatusError means the blob was not uploaded due to client or server
-	// errors and the corresponding record needs to be updated (either by
-	// retrying blob upload or deleting the entry).
-	StatusError
 )
 
 // BlobRef is a metadata document to keep track of blobs stored in an external blob store.
@@ -63,7 +27,7 @@ type BlobRef struct {
 	// Size is the byte size of the blob
 	Size int64
 	// Status is the current status of the blob
-	Status Status
+	Status
 	// StoreKey is the key of the store that the blob belongs to
 	StoreKey string
 	// RecordKey is the key of the record that the blob belongs to
@@ -120,36 +84,6 @@ func NewBlobRef(size int64, storeKey, recordKey string) *BlobRef {
 		RecordKey:  recordKey,
 		Timestamps: timestamps.New(),
 	}
-}
-
-// Ready changes Status to BlobRefStatusReady and updates Timestamps.
-// It returns an error if the current Status is not BlobRefStatusInitializing.
-func (b *BlobRef) Ready() error {
-	if b.Status != StatusInitializing {
-		return errors.New("Ready was called when Status is not Initializing")
-	}
-	b.Status = StatusReady
-	b.Timestamps.Update()
-	return nil
-}
-
-// MarkForDeletion marks the BlobRef as BlobRefStatusPendingDeletion and updates Timestamps.
-// Returns an error if the current Status is not BlobRefStatusReady.
-func (b *BlobRef) MarkForDeletion() error {
-	if b.Status != StatusInitializing && b.Status != StatusReady {
-		return errors.New("MarkForDeletion was called when Status is not either Initializing or Ready")
-	}
-	b.Status = StatusPendingDeletion
-	b.Timestamps.Update()
-	return nil
-}
-
-// Fail marks the BlobRef as BlobRefStatusError and updates Timestamps.
-// Any state can transition to BlobRefStatusError.
-func (b *BlobRef) Fail() error {
-	b.Status = StatusError
-	b.Timestamps.Update()
-	return nil
 }
 
 // ObjectPath returns an object path for the backend blob storage.
