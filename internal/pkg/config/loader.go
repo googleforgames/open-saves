12586e9ccb24e0a370b8d5d8d4288e45221a8254
Alex Hong <9397363+hongalex@users.noreply.github.com>
2022-10-13 12:57:27 -0700

    fix verbs for trace log

--- internal/pkg/config/loader.go
+++ internal/pkg/config/loader.go
@@ -90,7 +90,7 @@ func Load(path string) (*ServiceConfig, error) {
 	if viper.GetString(RedisAddress) == "" {
 		log.Fatal("missing -cache argument for cache store")
 	}
-	log.Printf("trace: %b, rate: %d\n", viper.GetBool(EnableTrace), viper.GetFloat64(TraceSampleRate))
+	log.Printf("trace: %t, rate: %f\n", viper.GetBool(EnableTrace), viper.GetFloat64(TraceSampleRate))
 
 	serverConfig := ServerConfig{
 		Address:             fmt.Sprintf(":%d", viper.GetUint(OpenSavesPort)),
