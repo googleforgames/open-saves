bfa58f5d8037b03bf81744da92347a9b9498bd20
Vini Vasconcelos <vinicius.vasconcelos@2k.com>
2024-05-21 21:55:44 -0300

    fix: remove wrong metadb traces

--- internal/app/server/open_saves.go
+++ internal/app/server/open_saves.go
@@ -32,10 +32,7 @@ import (
 	"github.com/googleforgames/open-saves/internal/pkg/metadb/record"
 	"github.com/googleforgames/open-saves/internal/pkg/metadb/store"
 	log "github.com/sirupsen/logrus"
-	"go.opentelemetry.io/contrib/instrumentation/google.golang.org/grpc/otelgrpc"
 	"google.golang.org/api/iterator"
-	"google.golang.org/api/option"
-	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 	empty "google.golang.org/protobuf/types/known/emptypb"
@@ -75,15 +72,7 @@ func newOpenSavesServer(ctx context.Context, cfg *config.ServiceConfig) (*openSa
 			return nil, err
 		}
 
-		//enable traces on Metadb ds integration.
-		var o []option.ClientOption
-		if cfg.EnableTrace {
-			o = append(o,
-				option.WithGRPCDialOption(grpc.WithUnaryInterceptor(otelgrpc.UnaryClientInterceptor())),
-				option.WithGRPCDialOption(grpc.WithStreamInterceptor(otelgrpc.StreamClientInterceptor())))
-		}
-
-		metadb, err := metadb.NewMetaDB(ctx, cfg.ServerConfig.Project, o...)
+		metadb, err := metadb.NewMetaDB(ctx, cfg.ServerConfig.Project)
 		if err != nil {
 			log.Fatalf("Failed to create a MetaDB instance: %v", err)
 			return nil, err
