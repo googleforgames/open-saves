dcc041933df71961a325990b867ab83b5fae8089
Zurvarian <zurvarian@gmail.com>
2025-06-10 09:54:20 +0200

    chore: Log the errors returned by the async-collector

--- internal/app/collector/async/server.go
+++ internal/app/collector/async/server.go
@@ -4,6 +4,7 @@ import (
 	"context"
 
 	"github.com/GoogleCloudPlatform/functions-framework-go/funcframework"
+	"github.com/cloudevents/sdk-go/v2/event"
 	log "github.com/sirupsen/logrus"
 )
 
@@ -15,9 +16,20 @@ func Run(ctx context.Context, cfg *Config) {
 		return
 	}
 
-	funcframework.RegisterCloudEventFunctionContext(ctx, "/", c.deleteBlobDependencies)
+	funcframework.RegisterCloudEventFunctionContext(ctx, "/", logErrorIfApply(c.deleteBlobDependencies))
 
 	if err := funcframework.Start(cfg.Port); err != nil {
 		log.Fatalf("funcframework.Start: %v\n", err)
+	}
+}
+
+func logErrorIfApply(chainFunc func(context.Context, event.Event) error) func(context.Context, event.Event) error {
+	return func(ctx context.Context, e event.Event) error {
+		err := chainFunc(ctx, e)
+		if err != nil {
+			log.Errorf("event %s failed with error: %v", string(e.Data()), err)
+		}
+
+		return err
 	}
 }
